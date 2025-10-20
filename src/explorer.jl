6b3fbc4d9bc6d3410f092bb396460f253037e800
jalmeida9457 <justin@thoughtmodeworks.com>
2025-10-20 17:53:51 -0400

    update interactive explorer

--- src/explorer.jl
+++ src/explorer.jl
@@ -10,10 +10,13 @@ Display DataFrame with pretty formatting
 function display_table(df::DataFrame; max_rows::Int=20)
     if nrow(df) > max_rows
         println("\nShowing first $max_rows of $(nrow(df)) rows:")
-        pretty_table(first(df, max_rows), crop=:horizontal, max_num_of_columns=10)
+        pretty_table(first(df, max_rows),
+            maximum_number_of_columns=10,
+            maximum_number_of_rows=max_rows)
         println("\n... $(nrow(df) - max_rows) more rows")
     else
-        pretty_table(df, crop=:horizontal, max_num_of_columns=10)
+        pretty_table(df,
+            maximum_number_of_columns=10)
     end
 end
 
